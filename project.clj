1abebefb3a080d35b69f1fe56ee91c098b3a8887
Henry Garner <henry@henrygarner.com>
2015-12-24 16:22:18 +0000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/stats "0.1.3"
+(defproject kixi/stats "0.2.0-SNAPSHOT"
   :description "A library of statistical transducing functions"
   :url "https://github.com/mastodonc/kixi.stats"
   :license {:name "Eclipse Public License"
