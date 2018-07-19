8689d5a7d21d850e84efc2716d2275df506281dc
Henry Garner <henry@henrygarner.com>
2018-07-19 13:10:22 +0100

    Release v0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/stats "0.4.1-SNAPSHOT"
+(defproject kixi/stats "0.4.1"
   :description "A library of statistical distribution sampling and transducing functions"
   :url "https://github.com/mastodonc/kixi.stats"
   :license {:name "Eclipse Public License"
@@ -14,7 +14,7 @@
                         [lein-codox "0.10.3"]
                         [lein-doo "0.1.6"]]}}
   :codox {:project {:name "kixi.stats"}
-          :source-uri "https://github.com/MastodonC/kixi.stats/blob/v0.4.0/{filepath}#L{line}"}
+          :source-uri "https://github.com/MastodonC/kixi.stats/blob/v0.4.1/{filepath}#L{line}"}
   :cljsbuild
   {:builds
    {:test {:source-paths ["src" "test"]
